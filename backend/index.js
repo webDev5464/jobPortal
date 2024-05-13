069cf607060b3bea14f4b2ceb50fd8eb9adf62ba
vishrutchakalasiya <vishrutchakalasiya@gmail.com>
2024-05-13 15:18:22 +0530

    mongo setup done

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const app = express()
+
+
+const mongodbCon = async (URL) => {
+    await mongoose.connect(URL)
+    console.log("Database COnnected");
+}
+
+mongodbCon("mongodb+srv://jobPortal:jobPortal@database.f2htg2a.mongodb.net/?retryWrites=true&w=majority&appName=database")
+
+app.use(express.json())
+app.use(cors())
+app.listen(5500,() => console.log('port is running on 5500'))
