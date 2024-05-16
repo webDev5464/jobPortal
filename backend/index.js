332f5e7a06af7a75fca23399c4de91d083c95f1a
jatssdev <167951806+jatssdev@users.noreply.github.com>
2024-05-16 18:14:31 +0530

    backend configurations, middlewares and authentications apis are ready by jatssdev

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const cors = require('cors')
 const { mongodbCon } = require('./configs/dbCon')
-
+const userRoutes = require('./routes/userRoutes')
 const app = express()
 app.use(cors())
 app.use(express.json())
+var cookies = require("cookie-parser");
 
+app.use(cookies());
+
+app.use('/api/user', userRoutes)
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+        success: false
+
+    });
+});
 
 
 
---
+++ backend/middleware/auth.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/Usermodel');
+const ErrorHandler = require('./errorHandler'); // Import the ErrorHandler middleware
+
+const authenticateUser = async (req, res, next) => {
+    try {
+        // Extract token from request headers
+        const token = req.cookies.token || req.headers.authorization;
+
+        // Check if token is provided=1
+        if (!token) {
+            throw new ErrorHandler('Token is required', 401);
+        }
+
+        // Verify token
+        const decoded = jwt.verify(token, 'jatinisbestcoderintheworld');
+
+        // Find user by id from decoded token
+        const user = await User.findById(decoded.userId);
+        if (!user) {
+            throw new ErrorHandler('User not found', 404);
+        }
+
+        // Attach user object to request for further processing
+        req.user = user;
+
+        // Proceed to next middleware or route handler
+        next();
+    } catch (error) {
+        next(error); // Pass errors to custom error handler
+    }
+};
+
+module.exports = { authenticateUser };
