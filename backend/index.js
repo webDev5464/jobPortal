788be222a506f90f19214afe36130aefbf75e093
Dev_eLoper <143530848+webDev5464@users.noreply.github.com>
2024-05-16 13:18:19 +0530

    branch check

--- backend/index.js
+++ backend/index.js
@@ -13,4 +13,6 @@ mongodbCon("mongodb+srv://jobPortal:jobPortal@database.f2htg2a.mongodb.net/?retr
 
 app.use(express.json())
 app.use(cors())
-app.listen(5500,() => console.log('port is running on 5500'))
+app.listen(5500, () => console.log('port is running on 5500'))
+
+// lorem
