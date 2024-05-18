eccf9bfc79c6d4037b47a0524c0c704f894026b0
Dev_eLoper <143530848+webDev5464@users.noreply.github.com>
2024-05-18 15:39:24 +0530

    customised color add.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,3 @@
 export default function App() {
-  return <h1 className="text-4xl m-5 font-bold text-red-500">Hello World!</h1>
+  return <h1 className="text-4xl m-5 font-bold text-primaryRed">Hello World!</h1>
 }
