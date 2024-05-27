a144ddd2405c8886add60fa485393a25f9ab630e
error-patel <152587167+error-patel@users.noreply.github.com>
2024-05-27 16:48:04 +0530

    atached footer in mainpage

--- client/src/components/Mainpage.jsx
+++ client/src/components/Mainpage.jsx
@@ -29,6 +29,7 @@ import { MdWhatsapp } from "react-icons/md";
 
 import { FaAngleDoubleUp } from "react-icons/fa";
 import TestimonialSwiper from './user/mianPage/TestimonialSwiper';
+import Footer from './user/Footer';
 
 
 
@@ -51,7 +52,7 @@ const Mainpage = () => {
 
                         <div><input className="placeholder:italic placeholder:text-slate-400  bg-white w-[300px] border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm h-[50px] w-[300px]" placeholder="Select Sector" type="text" name="search" /></div>
 
-                        <div className="w-[50px] h-[50px] text-white border border-yellow-600 border-4 rounded-md justify-center flex items-center text-[25px]">{<FaMagnifyingGlass />}</div>
+                        <div className="w-[50px] h-[50px] text-white  border-yellow-600 border-4 rounded-md justify-center flex items-center text-[25px]">{<FaMagnifyingGlass />}</div>
                     </div>
                     <div id='upload-hiring' className='flex justify-center items-center gap-3'>
                         <div className='flex  items-center border-2 p-2 rounded-sm text-white border-black bg-black/65'><span className='text-[29px]'>{<FaRegArrowAltCircleUp />}</span><p className='text-xl ml-1'>Upload Your Resume</p></div>
@@ -222,6 +223,7 @@ const Mainpage = () => {
                 </div>
 
             </section>
+            <Footer/>
 
 
 
